2e9d13dde5a1d57d2751ca1a54797d93892f61b0
Gilangdwi014 <203040014@mail.unpas.ac.id>
2022-10-17 14:52:54 +0700

    Menyelesaikan Video 3 (Struktur Folder, Routes & View laravel)

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,17 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+Route::get('/about', function () {
+    return view('about', [
+        "name" => "Gilang Dwi",
+        "email" => "203040014@mail.unpas.ac.id",
+        "image" => "gilang.jpg"
+    ]);
+});
+
+Route::get('/blog', function () {
+    return view('posts');
 });
