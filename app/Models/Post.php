35a34049a54f3922f99b3edd945127f16a370ac1
Gilangdwi014 <203040014@mail.unpas.ac.id>
2022-10-25 17:02:29 +0700

    Menyelesaikan Video 7 (Post Model Laravel)

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,32 +2,13 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
-class post
+class Post extends Model
 {
-    private static $blog_posts =
-    [
-        [
-            "title" => "Judul Post Pertama",
-            "slug" => "judul-post-pertama",
-            "author" => "Gilang",
-            "body" => "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quia dignissimos animi eaque aspernatur officiis deserunt ipsum dolorum nemo suscipit assumenda amet rerum praesentium, esse iure beatae, autem, eos distinctio fuga magni aliquam. Neque, iste tenetur voluptatibus quos itaque unde mollitia iusto, vero laborum doloremque asperiores, suscipit quae necessitatibus? Ipsum, officia esse eligendi quis ut suscipit, voluptates, dignissimos id saepe quae voluptatibus corrupti neque minus facere quam? Laudantium, expedita veniam necessitatibus voluptatibus, pariatur minima, quia et sint nesciunt tempora nulla ipsum?"
-        ],
-        [
-            "title" => "Judul Post Kedua",
-            "slug" => "judul-post-kedua",
-            "author" => "Dwi",
-            "body" => "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quia dignissimos animi eaque aspernatur officiis deserunt ipsum dolorum nemo suscipit assumenda amet rerum praesentium, esse iure beatae, autem, eos distinctio officia esse eligendi quis ut suscipit, voluptates, dignissimos id saepe quae voluptatibus corrupti neque minus facere quam? Laudantium, expedita veniam necessitatibus voluptatibus, pariatur minima, quia et sint nesciunt tempora nulla ipsum? fuga magni aliquam. Neque, iste tenetur voluptatibus q uos itaque unde mollitia iusto, vero laborum doloremque asperiores, suscipit quae necessitatibus? Ipsum, officia esse eligendi quis ut suscipit, voluptates, dignissimos id saepe quae voluptatibus corrupti neque minus facere quam? Laudantium, expedita veniam necessitatibus voluptatibus, pariatur minima, quia et sint nesciunt tempora nulla ipsum?"
-        ]
-    ];
+    use HasFactory;
 
-    public static function all()
-    {
-        return collect(self::$blog_posts);
-    }
-    public static function find($slug)
-    {
-        $posts = static::all();
-        return $posts->firstWhere('slug', $slug);
-    }
+    // protected $fillable = ['title', 'excerpt', 'body'];
+    protected $guarded = ['id'];
 }
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -4,11 +4,10 @@
     
     @foreach ($posts as $post)
     <article>
-    <h2>
-        <a href="/posts/{{ $post["slug"] }}">{{ $post["title"] }}</a>
-    </h2>
-    <h5>By: {{ $post["author"] }}</h5>
-    <p>{{ $post["body"] }}</p>
+    <h2><a href="/posts/{{ $post->slug }}">{{ $post->title }}</a> </h2>
+    
+    <p>{{ $post->excerpt }}</p>
+    
     </article>
     @endforeach
 
