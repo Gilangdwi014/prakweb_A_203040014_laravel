65847b6395b964903de911b80b7dac4d968f5d0e
Gilangdwi014 <203040014@mail.unpas.ac.id>
2022-10-25 21:58:36 +0700

    Menyelesaikan Video 8 (Post Category & Eloquent Relationship Laravel)

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -4,7 +4,10 @@
 
 <h1 class="mb-5">{{ $post->title }}</h1>
 
+<p>By. Gilang Dwi in <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a></p>
+
     {!! $post->body !!}
 
-<a href="/blog">Back to Post</a>
+<a href="/posts">Back to Post</a>
+
 @endsection
