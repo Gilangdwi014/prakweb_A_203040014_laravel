a2a613c08ac5702d5fa378d8ab45d3838568a860
Gilangdwi014 <203040014@mail.unpas.ac.id>
2022-10-31 12:47:42 +0700

    Menyelesaikan Video 13 (Searching & Pagination Laravel)

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -2,7 +2,26 @@
 
 @section('container')
 
-    <h1 class="mb-5">{{ $title }}</h1>
+    {{-- <h1 class="mb-5">{{ $title }}</h1> --}}
+
+    <h1 class="mb-3 text-center">{{$title}}</h1>
+
+    <div class="row justify-content-center mb-3">
+      <div class="col-md-6">
+        <form action="/posts" >
+          @if (request('category'))
+          <input type="hidden" name="category" value="{{request('category')}}">
+          @endif
+          @if (request('author'))
+          <input type="hidden" name="author" value="{{request('author')}}">
+          @endif
+          <div class="input-group mb-3">
+            <input type="text" class="form-control" placeholder="Search.." name="search" value="{{request('search')}}">
+            <button class="btn btn-primary" type="submit">Search</button>
+          </div>
+        </form>
+      </div>
+    </div>
 
     @if ($posts->count())
     
@@ -12,7 +31,7 @@
         <h3 class="card-title"><a class="text-decoration-none text-dark" href="/posts/{{$posts[0]->slug}}">{{$posts[0]->title}}</a></h3>
         <p>
           <small class="text-muted">
-            By. <a href="/authors/{{$posts[0]->author->username}}" class="text-decoration-none">{{$posts[0]->author->name}}</a> in <a class="text-decoration-none" href="/categories/{{ $posts[0]->category->slug }}">{{$posts[0]->category->name}}</a> {{$posts[0]->created_at->diffForHumans()}}
+            By. <a href="/posts?author={{$posts[0]->author->username}}" class="text-decoration-none">{{$posts[0]->author->name}}</a> in <a class="text-decoration-none" href="/posts?category={{ $posts[0]->category->slug }}">{{$posts[0]->category->name}}</a> {{$posts[0]->created_at->diffForHumans()}}
           </small>
         </p>
         <p class="card-text">{{$posts[0]->excerpt}}</p>
@@ -20,22 +39,20 @@
         <a class="text-decoration-none btn btn-primary" href="posts/{{$posts[0]->slug}}">Read More</a>
       </div>
     </div>
-  @else
-      <p class="text-center fs-4">No post found.</p>
-  @endif
+  
   
   <div class="container">
     <div class="row">
       @foreach ($posts->skip(1) as $post)
         <div class="col-md-4 mb-3">
           <div class="card">
-            <div class="position-absolute px-3 py-2 " style="background-color:rgba(0, 0, 0, 0.4)"><a class="text-white text-decoration-none" href="/categories/{{ $post->category->slug }}">{{$post->category->name}}</a></div>
+            <div class="position-absolute px-3 py-2 " style="background-color:rgba(0, 0, 0, 0.4)"><a class="text-white text-decoration-none" href="/posts?category={{ $post->category->slug }}">{{$post->category->name}}</a></div>
             <img src="https://source.unsplash.com/500x400?{{$post->category->name}}" class="card-img-top" alt="{{$post->category->name}}">
             <div class="card-body">
               <h5 class="card-title">{{$post->title}}</h5>
               <p>
                 <small class="text-muted">
-                  By. <a href="/authors/{{$post->author->username}}" class="text-decoration-none">{{$post->author->name}}</a> {{$post->created_at->diffForHumans()}}
+                  By. <a href="/posts?author={{$post->author->username}}" class="text-decoration-none">{{$post->author->name}}</a> {{$post->created_at->diffForHumans()}}
                 </small>
               </p>
               <p class="card-text">{{$post->excerpt}}</p>
@@ -46,5 +63,12 @@
       @endforeach
     </div>
   </div>
+
+  @else
+    <p class="text-center fs-4 fw-bold">No post found<br>Try to search another post</p>
+  @endif
+    <div class="d-flex justify-content-center">
+      {{$posts->links()}}
+    </div>
 
 @endsection
