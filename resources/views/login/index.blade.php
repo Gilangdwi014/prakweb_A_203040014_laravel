9eff64cd3c45283c0ba1e5cb59879bbd289f7298
Gilangdwi014 <203040014@mail.unpas.ac.id>
2022-11-01 17:25:02 +0700

    Menyelesaikan video 15 (User registration Laravel)

--- resources/views/login/index.blade.php
+++ resources/views/login/index.blade.php
@@ -5,15 +5,23 @@
 <div class="row justify-content-center">
     <div class="col-md-4">
     <main class="form-signin">
+
+      @if(session()->has('success'))
+          <div class="alert alert-success alert-dismissible fade show" role="alert">
+              {{ session('success') }}
+              <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+          </div>
+      @endif
+
         <h1 class="h3 mb-3 fw-normal text-center">Please login</h1>
         <form> 
           <div class="form-floating">
-            <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
-            <label for="floatingInput">Email address</label>
+              <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+              <label for="floatingInput">Email address</label>
           </div>
           <div class="form-floating">
-            <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
-            <label for="floatingPassword">Password</label>
+              <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
+              <label for="floatingPassword">Password</label>
           </div>
       
           <button class="w-100 btn btn-lg btn-primary" type="submit">Login</button>
