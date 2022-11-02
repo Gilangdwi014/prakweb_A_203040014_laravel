434f049af8375a6591cd73c52db787341aaddca1
Gilangdwi014 <203040014@mail.unpas.ac.id>
2022-11-02 10:06:07 +0700

    Menyelesaikan video 16 (User login & Middleware Laravel)

--- resources/views/login/index.blade.php
+++ resources/views/login/index.blade.php
@@ -4,7 +4,6 @@
 
 <div class="row justify-content-center">
     <div class="col-md-4">
-    <main class="form-signin">
 
       @if(session()->has('success'))
           <div class="alert alert-success alert-dismissible fade show" role="alert">
@@ -13,15 +12,30 @@
           </div>
       @endif
 
-        <h1 class="h3 mb-3 fw-normal text-center">Please login</h1>
-        <form> 
-          <div class="form-floating">
-              <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
-              <label for="floatingInput">Email address</label>
+      @if(session()->has('loginError'))
+          <div class="alert alert-danger alert-dismissible fade show" role="alert">
+              {{ session('loginError') }}
+              <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
           </div>
+      @endif
+        
+        <main class="form-signin w-100 m-auto">
+        <h1 class="h3 mb-3 fw-normal text-center">Please login</h1>
+        <form action="/login" method="post">
+          @csrf
           <div class="form-floating">
-              <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
-              <label for="floatingPassword">Password</label>
+            <input type="email" class="form-control @error('email') is-invalid @enderror" name="email" id="email" au placeholder="name@example.com" autofocus required value="{{ old('email') }}">
+            <label for="email" au>Email address</label>
+            @error('email')
+                <div class="invalid-feedback">
+                    {{ $message }}
+                </div>
+            @enderror
+          </div>
+
+          <div class="form-floating">
+            <input type="password" name="password" class="form-control" id="password" placeholder="Password" required>
+            <label for="password">Password</label>
           </div>
       
           <button class="w-100 btn btn-lg btn-primary" type="submit">Login</button>
